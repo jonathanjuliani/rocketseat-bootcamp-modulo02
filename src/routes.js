94cb6bc0f0180b03bc97663c556b6172856a5906
Jonathan Juliani <jonathanjuliani@outlook.com>
2019-08-27 00:33:29 -0300

    update - controller e conexao ao db

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
+// import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello world' });
-});
+// routes.get('/', async (req, res) => {
+//   const response = await User.create({
+//     name: 'Jonathan Juliani',
+//     email: 'jonathanjuliani@outlook.com',
+//     password_hash: '123456789',
+//   });
+//   return res.json(response);
+// });
+
+routes.post('/users', UserController.store);
 
 export default routes;
