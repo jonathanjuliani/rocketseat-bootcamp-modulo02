37a1aba755fa2e69adae74c0bd4c03fca9a97b44
Jonathan Juliani <jonathan.juniani@userede.com.br>
2019-08-27 17:33:34 -0300

    update - init do upload files e relac user x file

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,13 @@
 import { Router } from 'express';
-// import User from './app/models/User';
+import multer from 'multer';
+import multerConfig from './configs/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
-
-// routes.get('/', async (req, res) => {
-//   const response = await User.create({
-//     name: 'Jonathan Juliani',
-//     email: 'jonathanjuliani@outlook.com',
-//     password_hash: '123456789',
-//   });
-//   return res.json(response);
-// });
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -21,5 +15,6 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
