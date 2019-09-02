74a082c95eb5ff495bd3b221d769edbed6060c1d
Jonathan Juliani <jonathan.juniani@userede.com.br>
2019-09-02 17:28:16 -0300

    update

---
+++ src/app/jobs/CancellationMail.js
@@ -0,0 +1,13 @@
+class CancellationMail {
+  get key() {
+    return 'CancellationMail';
+  }
+
+  async handle() {
+    // Mail.sendMain({
+    //   to: ``
+    // })
+  }
+}
+
+export default new CancellationMail();
