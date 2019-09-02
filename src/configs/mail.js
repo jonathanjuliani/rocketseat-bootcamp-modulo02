c54608c0e644ffe11643b8e9b28804159c40eae2
Jonathan Juliani <jonathanjuliani@outlook.com>
2019-09-02 06:51:14 -0300

    update - adding nodemailer

---
+++ src/configs/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'bce1dc67247d99',
+    pass: 'd48dc5a4469b44',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
